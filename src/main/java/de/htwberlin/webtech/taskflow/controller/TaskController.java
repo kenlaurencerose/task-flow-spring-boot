b942d48c33ad667ec88745f6345d9f5928ad0f95
kenlaurencerose <kenlaurencerose@gmail.com>
2026-04-22 10:42:15 +0200

    Milestone1 fertig

--- src/main/java/de/htwberlin/webtech/taskflow/controller/TaskController.java
+++ src/main/java/de/htwberlin/webtech/taskflow/controller/TaskController.java
@@ -4,22 +4,22 @@ import de.htwberlin.webtech.taskflow.entity.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class TaskController {
 
-    @GetMapping("/tasks")
-    public String getTasks(Model model){
+    @GetMapping("/")
+    public List<Task> getTasks(Model model){
     List<Task> tasks = List.of(
                 new Task(1L, "Projekt anlegen", "SpringProjekt erstellen", "Open", "High"),
                 new Task(2L, "GetRoute bauen", "Route für TaskListe erstellen", "inProgress", "High"),
                 new Task(3L, "Repo pushen", "Code auf GitHub hochladen", "Done", "High")
                 );
 
-        model.addAttribute("tasks", tasks);
-        return "tasks";
+        return tasks;
 
     }
 }
